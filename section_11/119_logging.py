32e78a76a81eabdb1b6f7b9c63e8adf9e0a23b6d
colra03 <C0o3k0e3??>
2018-11-26 23:35:51 +0900

    section11_117~119

---
+++ section_11/119_logging.py
@@ -0,0 +1,20 @@
+"""
+CRITICAL
+ERROR
+WRANING
+INFO
+DEBUG
+"""
+import logging
+
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
+# logging.critical('critical')
+# logging.error('error')
+# logging.warning('warning')
+# logging.info('info')
+# logging.debug('debug')
+
+# logging.info('info {}'.format('test'))
+
+# logging.info('info %s %s' % ('test', 'test2'))
+logging.info('info %s %s', 'test', 'test2')
